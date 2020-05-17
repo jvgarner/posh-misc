a314618ef00c62825623403dc5790a2603ea31ab
Jason Garner <j.v.garner@gmail.com>
2020-05-16 20:03:13 -0400

    Updated

--- src/ConvertTo-Gif.psm1
+++ src/ConvertTo-Gif.psm1
@@ -18,13 +18,26 @@ function ConvertTo-Gif {
         [int]$Scale = 480,
 
         [Parameter(ValueFromPipeline = $false, Mandatory = $false)]
-        [int]$Fps = 15
+        [int]$Fps = 15,
+
+        [Parameter(ValueFromPipeline = $false, Mandatory = $false)]
+        [string]$SubtitleFilename
 
     )
     $palette="palette${(New-Guid)}.png"
     $filters="fps=$Fps,scale=${Scale}:-1:flags=lanczos"
-    ffmpeg -v warning -ss $StartTime -t $Duration -i $InputFile -vf "$filters,palettegen" -y $palette
-    ffmpeg -v warning -ss $StartTime -t $Duration -i $InputFile -i $palette -lavfi "$filters [x]; [x][1:v] paletteuse" -y $OutputFile
+    if ($srtFile) {
+        $filters = "$filters,subtitles='$SubtitleFilename':force_style='Fontsize=24'"
+        ffmpeg -v warning -ss $StartTime -t $Duration -i $InputFile -vf "$filters,palettegen" -y $palette
+        ffmpeg -v warning -i $InputFile -i $palette -ss $StartTime -t $Duration -lavfi "$filters [x];[x][1:v] paletteuse" -y $OutputFile
+    } else {
+        ffmpeg -v warning -ss $StartTime -t $Duration -i $InputFile -vf "$filters,palettegen" -y $palette
+        ffmpeg -v warning -ss $StartTime -t $Duration -i $InputFile -i $palette -lavfi "$filters [x];[x][1:v] paletteuse" -y $OutputFile
+    }
+    # $filters="fps=$Fps,scale=${Scale}:-1:flags=lanczos,subtitles='sub.srt':force_style='Fontsize=24'"
+    #ffmpeg -v warning -i '.\Black Books 1x2 - Manny''s First Day.mkv' -ss 2:59 -t 12 -vf "subtitles='sub.srt':force_style='Fontsize=24'" -y out.mp4
+
+    
     Remove-Item $palette 
 }
 
