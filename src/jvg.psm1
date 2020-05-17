b4315817ce7db52c5a6731aa2bcbddd7812ee0c8
Jason Garner <1078233+jvgarner@users.noreply.github.com>
2020-05-16 20:03:13 -0400

    Updated

--- src/jvg.psm1
+++ src/jvg.psm1
@@ -16,21 +16,24 @@ function Invoke-Weather {
 
 # TODO: Cleanup script - empty recycle bin, remove downloads, what else?
 
-function Backup-UserData {
-    Param(
-        [switch]$ExternalBackup
-    )
-    $timestamp = Get-FilenameSafeTimeStamp
+# To backup: 
+# docker config?
+# visual studio config?
 
-    if($externalBackup){
-        
-        Robocopy.exe x:\usr\   d:\usr\   /MIR /R:5 /W:5 /MT:32 /NP /UNILOG+:c:\Logs\Backups\x-usr-$timestamp.log 
-        Robocopy.exe x:\media\ d:\media\ /MIR /R:5 /W:5 /MT:32 /NP /UNILOG+:c:\Logs\Backups\x-media-$timestamp.log 
-    } else {
-        Robocopy.exe C:\usr\ X:\usr\     /MIR /R:5 /W:5 /MT:32 /NP /UNILOG+:c:\Logs\Backups\c-usr-$timestamp.log 
-        $target = "X:\usr\jvg\UserDirectory"
-        Robocopy.exe $HOME $target /MIR /R:5 /W:5 /MT:32 /NP /XJD /XA:SH /XD AppData IISExpress .vscode .nuget .android .atom .dotnet .docker .eclipse .templateengine docker .omnisharp /UNILOG+:c:\Logs\Backups\c-usr-$timestamp.log 
-    }
+function Backup-UserData {
+    Copy-Item C:\Users\jvg\APPDATA\Roaming\Code\User\settings.json c:\usr\jvg\backup\vscode\settings.json -Force
+    Copy-Item C:\Users\jvg\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json c:\usr\jvg\backup\windows-terminal\settings.json -Force
+    code --list-extensions > c:\usr\jvg\backup\vscode\extensions-list.txt
+    choco list -lo > c:\usr\jvg\backup\choco\installed-list.txt 
+
+    7z.exe a -t7z c:\usr\jvg\backup\plex.7z 'C:\Users\jvg\AppData\Local\Plex Media Server\' -mx0 -xr!Cache -xr!Logs
+    7z.exe a -t7z c:\usr\jvg\backup\Documents.7z 'C:\Users\jvg\Documents\' -mx0
+
+    $timestamp = Get-FilenameSafeTimeStamp
+    Robocopy.exe c:\usr\ d:\usr\ /MIR /Z /R:5 /W:5 /MT:32 /NP /UNILOG+:c:\Logs\Backups\c-usr-to-d-$timestamp.log
+
+    Remove-Item c:\usr\jvg\backup\Documents.7z -Force
+    Remove-Item c:\usr\jvg\backup\plex.7z -Force
 }
 
 Set-Alias jvgBackup Backup-UserData 
