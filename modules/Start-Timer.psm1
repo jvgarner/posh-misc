830496cc5b8bcbb7b6f1591cc55d3a735db59882
Jason Garner <1078233+jvgarner@users.noreply.github.com>
2025-10-30 13:13:39 -0400

    Add Start-Timer module

---
+++ modules/Start-Timer.psm1
@@ -0,0 +1,30 @@
+<#
+.SYNOPSIS
+  Start a timer in the background.
+.PARAMETER TimeSpanText
+  The length of time to set the timer for. Accepts values like '1h30m15s', '45m', or '10s', etc.
+.PARAMETER Message
+  The message to display when the timer is complete.
+#>
+function Start-Timer {
+    param (
+        [Parameter(Mandatory, Position = 0)]
+        [string]
+        $TimeSpanText,
+
+        [Parameter()]
+        [string]
+        $Message = "Timer complete!"
+    )
+    
+    $duration = ConvertTo-TimeSpan $TimeSpanText;
+    Start-Job -ScriptBlock { 
+        param($duration, $Message)
+        Start-Sleep -Duration $duration
+        $dismissButton = New-BTButton -Content "Dismiss" -Arguments "dismiss"
+        New-BurntToastNotification -Text $Message -Button $dismissButton
+    } -ArgumentList $duration, $Message | Out-Null
+}
+
+New-Alias timer Start-Timer
+Export-ModuleMember -Function * -Alias *
