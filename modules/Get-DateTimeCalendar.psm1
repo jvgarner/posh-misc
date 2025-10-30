f04ed03e60bb9b3694fa02201c2011dd9e9ae9c7
Jason Garner <1078233+jvgarner@users.noreply.github.com>
2025-10-30 09:57:17 -0400

    Fix xml doc

--- modules/Get-DateTimeCalendar.psm1
+++ modules/Get-DateTimeCalendar.psm1
@@ -1,8 +1,22 @@
 <#
 .SYNOPSIS
-    Show current date and time and calendar.
+Show current date and time and calendar.
 .EXAMPLE
-    Get-DateTimeCalendar
+Get-DateTimeCalendar
+Output:
+Thursday, October 30, 2025 9:13:10 AM
+
+               October 2025
+
+ Sun   Mon   Tue   Wed   Thu   Fri   Sat
+  28    29    30     1     2     3     4
+   5     6     7     8     9    10    11
+  12    13    14    15    16    17    18
+  19    20    21    22    23    24    25
+  26    27    28    29    30    31     1
+.LINK
+Get-Date
+Get-Calendar
 #>
 Function Get-DateTimeCalendar
 {
