d8d1c696fec588eeff3a707c0ee0e3fea5343b4c
Jason Garner <1078233+jvgarner@users.noreply.github.com>
2025-10-30 11:39:34 -0400

    Fix docs, add parameter alias

--- modules/Get-IsoDate.psm1
+++ modules/Get-IsoDate.psm1
@@ -1,16 +1,25 @@
 <#
 .SYNOPSIS
-Get the current date in ISO format
-
-.DESCRIPTION
-Get the current date in ISO format eg YYYY-MM-DD
-
+  Get the current date in ISO format eg YYYY-MM-DD
+.PARAMETER Date
+  Override the return date.
+.OUTPUTS
+  System.String
+.EXAMPLE
+  Get-IsoDate
+  Output: 2025-10-25
+.EXAMPLE 
+  Get-IsoDate '2010-4-5'
+  Output: 2010-04-05
+.EXAMPLE 
+  Get-IsoDate 'jan 5'
+  Output: 2010-01-05
 #>
-
 function Get-IsoDate 
 {
     Param(
         [Parameter(ValueFromPipeline = $true, Position = 0)]
+        [Alias("d")]
         [DateTime]$Date = (Get-Date)
     )
     $Date.ToString('yyyy-MM-dd')
