bcb8bda117688bcd8c0331ffd0b0a93c5f7795a7
Jason Garner <1078233+jvgarner@users.noreply.github.com>
2025-10-30 11:49:23 -0400

    Fix param and docs

--- modules/Get-FilenameSafeTimestamp.psm1
+++ modules/Get-FilenameSafeTimestamp.psm1
@@ -5,23 +5,33 @@
   Create a string that can be used in a filename from a DateTime.
 .PARAMETER Date
   The date and time to use - defaults to current.
-.PARAMETER ReplacementValue
-  The string to replace instances of ':' with.
+.PARAMETER Delimiter
+  The string to replace instances of ':' with - defaults to an empty string.
 .INPUTS
   DateTime
 .OUTPUTS
   String
+.EXAMPLE
+  Get-FileNameSafeTimeStamp
+  Output: 2025-10-30T114625.2337270-0400
+.EXAMPLE
+  Get-FileNameSafeTimeStamp -Delimiter '.'
+  Output: 2025-10-30T11.47.56.8767106-04.00
+.EXAMPLE 
+  fsts -d 🕛
+  Output: 2025-10-30T11🕛49🕛04.4076587-04🕛00
 #>
 function Get-FileNameSafeTimeStamp {
     Param(
         [Parameter(ValueFromPipeline = $true, Position = 0)]
-        [DateTime]$Date = (Get-Date),
+        [DateTime]$DateTime = (Get-Date),
 
         [Parameter(Mandatory=$false)]
-        [string]$ReplacementValue = '.'
+        [Alias("d")]
+        [string]$Delimiter = ''
     )
 
-    return $Date.ToString('o').Replace(':', $ReplacementValue)
+    return $DateTime.ToString('o').Replace(':', $Delimiter)
 }
 
 New-Alias fsts Get-FileNameSafeTimeStamp
