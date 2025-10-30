ce879085fef57485e3961adfb3fb48b1fd2e21e2
Jason Garner <1078233+jvgarner@users.noreply.github.com>
2025-10-30 11:25:24 -0400

    Fix docs

--- modules/Get-Guid.psm1
+++ modules/Get-Guid.psm1
@@ -1,21 +1,19 @@
 <#
 .SYNOPSIS
-Generate a version 4 UUID string.
-.DESCRIPTION
-Generate a version 4 UUID string. The guid generated is a Version 4 UUID (Universally Unique Identifier).
+  Generate a version 4 UUID string. The guid generated is a Version 4 UUID (Universally Unique Identifier).
 .OUTPUTS
-System.String
+  System.String
 .PARAMETER UpperCase
-Output the guid with uppercase letters.
+  Output the guid with uppercase letters.
 .EXAMPLE
-Get-Guid
-Output: 307d054a-5698-4c1e-9016-056c93fb5b29
+  Get-Guid
+  Output: 307d054a-5698-4c1e-9016-056c93fb5b29
 .EXAMPLE
-Get-Guid -UpperCase
-Output: 8D2A76B4-87B2-4E11-B601-A17478D581BE
+  Get-Guid -UpperCase
+  Output: 8D2A76B4-87B2-4E11-B601-A17478D581BE
 .EXAMPLE
-guid -u
-Output: 224CFCAD-0661-4052-80E9-C8C50FF81311
+  guid -u
+  Output: 224CFCAD-0661-4052-80E9-C8C50FF81311
 #>
 function Get-Guid {
     Param(
