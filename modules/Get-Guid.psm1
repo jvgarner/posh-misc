c56c77cc01e2092c2a2552c97a38d2fdf7cd4228
Jason Garner <1078233+jvgarner@users.noreply.github.com>
2025-10-30 09:54:58 -0400

    Add -UpperCase parameter and fix xml documentation

--- modules/Get-Guid.psm1
+++ modules/Get-Guid.psm1
@@ -1,11 +1,34 @@
 <#
 .SYNOPSIS
-    Generate a guid.
+Generate a version 4 UUID string.
+.DESCRIPTION
+Generate a version 4 UUID string. The guid generated is a Version 4 UUID (Universally Unique Identifier).
+.OUTPUTS
+System.String
+.PARAMETER UpperCase
+Output the guid with uppercase letters.
 .EXAMPLE
-    Get-Guid
+Get-Guid
+Output: 307d054a-5698-4c1e-9016-056c93fb5b29
+.EXAMPLE
+Get-Guid -UpperCase
+Output: 8D2A76B4-87B2-4E11-B601-A17478D581BE
+.EXAMPLE
+guid -u
+Output: 224CFCAD-0661-4052-80E9-C8C50FF81311
 #>
 function Get-Guid {
-    [guid]::NewGuid().ToString().ToUpperInvariant()
+    Param(
+        [Parameter(Mandatory = $false)]
+        [Alias("u")]
+        [switch]$UpperCase
+    )
+    
+    if($UpperCase)
+    {
+        return [guid]::NewGuid().ToString().ToUpperInvariant()
+    }
+    return [guid]::NewGuid().ToString()
 }
 
 New-Alias -Name guid -value Get-Guid
