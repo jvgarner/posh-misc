e2da027a9e65db057b2b21fbe430dbee75d1e5c2
Jason Garner <1078233+jvgarner@users.noreply.github.com>
2025-10-30 11:33:13 -0400

    Fix docs

--- modules/Get-Paths.psm1
+++ modules/Get-Paths.psm1
@@ -1,12 +1,18 @@
 <#
 .SYNOPSIS
-Get path vars as a list.
-
-.DESCRIPTION
-Get path vars as a list.
-
+  Get path environment variable split into one per line.
+.OUTPUTS
+  System.String
+.EXAMPLE
+  Get-Paths
+  Output:
+    C:\Program Files\PowerShell\7
+    C:\WINDOWS\system32
+    C:\WINDOWS
+    C:\WINDOWS\System32\Wbem
+    C:\WINDOWS\System32\WindowsPowerShell\v1.0\
+    # etc
 #>
-
 function Get-Paths
 { 
     $env:path -split ";" 
