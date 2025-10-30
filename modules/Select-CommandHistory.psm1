9f0329c9045edda85b6ebb85f0dc675592e4077d
Jason Garner <1078233+jvgarner@users.noreply.github.com>
2025-10-30 11:54:38 -0400

    Fix param and docs

--- modules/Select-CommandHistory.psm1
+++ modules/Select-CommandHistory.psm1
@@ -1,14 +1,37 @@
+<#
+.SYNOPSIS
+  Search the command history.
+.PARAMETER Keyword
+  The keyword to search for in the command history.
+.PARAMETER MaxResults
+  The maximum number of results to return - defaults to 10.
+.INPUTS
+  System.String
+.OUTPUTS
+  System.String
+.EXAMPLE
+  "git" | sh
+  Output:
+    winget install git.git
+    git
+    git status
+    # etc
+.EXAMPLE
+  Select-CommandHistory "git" 1
+  Output:
+    winget install git.git
+#>
 function Select-CommandHistory {
     Param(
-        [Parameter(ValueFromPipeline = $false, Mandatory = $true, Position = 0)]
-        [string]$SearchString,
+        [Parameter(ValueFromPipeline = $true, Mandatory = $true, Position = 0)]
+        [string]$Keyword,
 
         [Parameter(ValueFromPipeline = $false, Mandatory = $false, Position = 1)]
         [int]$MaxResults = 10
     )
     
     $historyPath = (Get-PSReadlineOption).HistorySavePath
-    $list = Get-Content $historyPath | Select-String $SearchString
+    $list = Get-Content $historyPath | Select-String $Keyword
 
     $list | Select-Object -First $MaxResults
 
