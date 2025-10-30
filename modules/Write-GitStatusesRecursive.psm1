001a77424c1f8680fedfedd0cab4866aa67473c5
Jason Garner <1078233+jvgarner@users.noreply.github.com>
2025-10-30 11:23:26 -0400

    Improve docs and check for posh-git

--- modules/Write-GitStatusesRecursive.psm1
+++ modules/Write-GitStatusesRecursive.psm1
@@ -1,27 +1,43 @@
 <#
 .SYNOPSIS
-Write the git status for every git repository found within the current directory.
-
-.DESCRIPTION
-Write the git status for every git repository found within the current directory.
-
+  Write the git status for every git repository recursively found within the current directory. Requires posh-git to be installed.
+.PARAMETER Fetch
+  Executes a "git fetch --all" on every git repository before writing out the status.
 .EXAMPLE
-Write-GitStatuses
-
-.NOTES
-Requires posh-git to be installed
+  D:\source\Write-GitStatuses
+  Output:
+  D:\source\Alpha0\ [main ↓1]
+  D:\source\Beta\ [test +1 ~3 -1 !]
+  D:\source\Gamma\ [main ≡]
+  D:\source\Zeta\ [main ↓5]
 #>
-function Write-GitStatusesRecursive {
+function Write-GitStatuses {
+  Param(
+    [Parameter(Mandatory = $false)]
+    [Alias("f")]
+    [Switch]$Fetch = $false
+  )
+
+    $requiredModule = 'Posh-Git'
+    if (-not (Get-Module -ListAvailable -Name $requiredModule)) {
+        Write-Error "Required module '$requiredModule' is not installed. Aborting execution."
+        return
+    }
+
     $startDir = Get-Location
     Get-ChildItem . -Recurse -Directory -Force | Where-Object Name -eq '.git' | 
         ForEach-Object { 
             $loc = $_.FullName.TrimEnd('.git')
             Set-Location $loc > $null;
+            if($Fetch) {
+                # Write-Host "$loc git fetch --all"
+                git fetch --all
+            }
             Write-Host $loc -NoNewLine 
-            Write-GitStatus (Get-GitStatus)
+            Write-VcsStatus
         }
     Set-Location $startDir 
 }
 
-New-Alias gits Write-GitStatusesRecursive
+New-Alias gits Write-GitStatuses
 Export-ModuleMember -Function * -Alias *
